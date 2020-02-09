d0e89df5d988102f183927c8e1bb2f77bbfcaedf
natael <natael.baffou1@gmail.com>
2020-02-09 23:50:34 +0100

    Tests et infimes modifs réalisées

--- src/Karting.java
+++ src/Karting.java
@@ -18,10 +18,10 @@ public class Karting extends Vehicule{
 	private double dt = 1;
 	private double masse = 10;
 	private double F = 1.25; // force de frottement des roues
+	private Roue[] roues;
 
 	public Karting(int x, int y){
-		this.x = x;
-		this.y = y;
+		this.P = new Position(x, y);
 		this.vx = 0;
 		this.vy = 0;
 		
@@ -32,26 +32,27 @@ public class Karting extends Vehicule{
 			normes[i] = Math.sqrt(tx[i]*tx[i]+ty[i]*ty[i]);
 			angles[i] = Math.atan2(ty[i], tx[i]);
 		}
+
+		roues = new Roue[4];
+		roues[0] = new Roue(10, 10);
+		roues[1] = new Roue(10,-10);
+		roues[2] = new Roue(-10, -10);
+		roues[3] = new Roue(-10,10);
 		
 	}
 
 	public void avancer(){
-		x += vx*dt;
-		y += vy*dt;
+		P.x += vx*dt;
+		P.y += vy*dt;
 	}
 	
 	
-	private void accelerer(int sens){
-		vx -= sens*Math.sin(alpha);
-		vy += sens*Math.cos(alpha);
-	}
-
-	public void accelerer(){
-		this.accelerer(1);
-	}
-
-	public void freiner(){
-		this.accelerer(-1);
+	public void accelerer(boolean av, boolean ar){
+		int sens = 0;
+		if(av && !ar){ sens = 1;}
+		if(ar && !av){ sens = -1;}
+		vx -= sens*Math.sin(P.getRad());
+		vy += sens*Math.cos(P.getRad());
 	}
 	
 	public void ralentir(){
@@ -61,25 +62,47 @@ public class Karting extends Vehicule{
 		if(Math.abs(vy) < 0.01){ vy = 0; }
 	}
 
-	public void tournerADroite(){
-		this.alpha += 0.1;
-	}
+	public void tourner(boolean g, boolean d){
+		int sens = 0;
+		if(g && !d){
+			sens = -1;
+			roues[0].tournerAGauche();
+			roues[3].tournerAGauche();
+		} else if(d && !g){
+			sens = 1;
+			roues[0].tournerADroite();
+			roues[3].tournerADroite();
+		} else {
+			roues[0].toutDroit();
+			roues[3].toutDroit();
+		}
 
-	public void tournerAGauche(){
-		this.alpha -= 0.1;
+		P.setRad(P.getRad()+sens*0.1);
+
 	}
 
 	public void dessine(Graphics g){
+		roues[0].dessine(g, P, normes[1], angles[1]);
+		roues[1].dessine(g, P, normes[2], angles[2]);
+		roues[2].dessine(g, P, normes[3], angles[3]);
+		roues[3].dessine(g, P, normes[4], angles[4]);
+
 		g.setColor(Color.black);
-		int[] posX = new int[5];
-		int[] posY = new int[5];
-		for(int i=0; i < 5; i++){
-			posX[i] = x + (int)(normes[i] * Math.cos(alpha + angles[i]));
-			posY[i] = y + (int)(normes[i] * Math.sin(alpha + angles[i]));
+		int[] posX = new int[tx.length];
+		int[] posY = new int[tx.length];
+		for(int i=0; i < tx.length; i++){
+			posX[i] = P.x + (int)(normes[i] * Math.cos(P.getRad() + angles[i]));
+			posY[i] = P.y + (int)(normes[i] * Math.sin(P.getRad() + angles[i]));
 		}
 		g.fillPolygon(posX, posY, posX.length);
+
 	}
-	
 
+	public double[] getNormes(){
+		return normes;
+	}
 
+	public double[] getAngles() {
+		return angles;
+	}
 }
--- src/Roue.java
+++ src/Roue.java
@@ -1,21 +1,48 @@
 import java.awt.*;
 
 public class Roue {
-    private double orientation = 0;
+    // position
+    private Position P;
+
+    // coordonnées de la roue centrée en (0, 0)
+    // image de la roue
+    private int tx[] = {3, 3, -3, -3};
+    private int ty[] = {-4, 4, 4, -4};
+    private double normes[];
+    private double angles[];
+
+    public Roue(int x, int y){
+        P = new Position(x, y);
+
+        normes = new double[tx.length];
+        angles = new double[tx.length];
+
+        for(int i=0; i < tx.length; i++){
+            normes[i] = Math.sqrt(tx[i]*tx[i]+ty[i]*ty[i]);
+            angles[i] = Math.atan2(ty[i], tx[i]);
+        }
+    }
 
     public void tournerAGauche(){
-        orientation = 45;
+        P.setDeg(-35);
     }
 
     public void tournerADroite(){
-        orientation = -45;
+        P.setDeg(35);
     }
 
     public void toutDroit(){
-        orientation = 0;
+        P.setDeg(0);
     }
 
-    public void dessine(Graphics g){
-
+    public void dessine(Graphics g, Position kartP, double kartNorme, double kartAngle){
+        g.setColor(Color.red);
+        int[] posX = new int[tx.length];
+        int[] posY = new int[tx.length];
+        for(int i=0; i < tx.length; i++){
+            posX[i] = kartP.x + (int)( kartNorme*Math.cos(kartP.getRad()+kartAngle) + normes[i]*Math.cos(kartP.getRad()+angles[i]+P.getRad()) );
+            posY[i] = kartP.y + (int)( kartNorme*Math.sin(kartP.getRad()+kartAngle) + normes[i]*Math.sin(kartP.getRad()+angles[i]+P.getRad()) );
+        }
+        g.fillPolygon(posX, posY, posX.length);
     }
 }
